3949f2e6715ac9d258971cd85580d90505b6057e
adit0004 <adit0004@student.monash.edu>
2021-04-23 00:59:50 +1000

    Initial code attempt

--- longestIncreasingSequence.py
+++ longestIncreasingSequence.py
@@ -1,2 +1,19 @@
 def longestSequence(string):
-    pass
+    numbers = string.split(" ")
+    maxLength = 1
+    maxStart = 0
+    curStart = 0
+    curLength = 1
+    for i in range(1,len(numbers)):
+        if numbers[i] <= numbers[i-1]:
+            if curLength > maxLength:
+                maxLength = curLength
+                maxStart = curStart
+            curStart = i
+            curLength = 1
+        else: 
+            curLength += 1
+    if curLength > maxLength:
+        maxLength = curLength
+        maxStart = curStart
+    return " ".join(numbers[maxStart:maxStart+maxLength])
